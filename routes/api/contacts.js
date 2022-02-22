d45e8dcf78ac62fef40b4b225593da5e6b85e1ff
MaksimEdam <Edamenko6@gmail.com>
2022-02-22 11:35:31 +0200

    remove contact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,15 +8,28 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactModel.getContactById(req.params.contactId);
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactModel.addContact(req.body);
+  res.status(201).json({ status: "success", code: 201, payload: { contact } });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactModel.removeContact(req.params.contactId);
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
